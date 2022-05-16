a11e1a9fe00cf0f3b72803c8e979a3946c0c84d1
Aftar Ahmad Sami <aftarahmadsami@gmail.com>
2022-05-17 01:14:29 +0600

    Practice

--- Go/testRun.go
+++ Go/testRun.go
@@ -1,9 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("Hello, ")
+	//var name string
+
+	fmt.Print("\tWelcome\n\n")
+	fmt.Println("What is your name?")
+	fmt.Print("--> ")
+
+	//------Input String with white spaces--------//
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	name := scanner.Text()
+
+	fmt.Println()
+
+	fmt.Printf("Hello, %s   :)\n\n", name) // USE Printf for formatting
+	//fmt.Println("Hello, " + name + "  :)")
+	//fmt.Println("\nHello, ", name)
 
 }
