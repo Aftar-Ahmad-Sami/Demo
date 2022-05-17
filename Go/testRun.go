f733791414c2a6c6af3b2df27b76ccf0f033061a
Aftar Ahmad Sami <aftarahmadsami@gmail.com>
2022-05-17 19:51:06 +0600

    updated

--- Go/testRun.go
+++ Go/testRun.go
@@ -4,6 +4,20 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("... Ready for Test Run? ....")
+	//	fmt.Println("... Ready for Test Run? ....")
+	var num int
+	fmt.Print("Enter Upper Bound:\t")
+	fmt.Scanln(&num)
+
+	fmt.Println()
+	for i := 1; i <= num; i++ {
+		if i%2 == 0 && i%3 == 0 {
+			fmt.Println(i, "\t- Divisible by 2 and 3")
+		} else if i%2 == 0 {
+			fmt.Println(i, "\t- Divisible by 2")
+		} else {
+			fmt.Println(i, "\t- Divisible by 3")
+		}
+	}
 
 }
